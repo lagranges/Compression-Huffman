dc972376b7bf5dad29f352112334bad9c4a45a74
lagranges <levantuan2312@gmail.com>
2016-11-21 15:57:58 +0100

    Ajouté le méthode Creer_Arbre(String) à partir d'un fichier textuel, le méthode Creer_Feulle()

--- huffman.adb
+++ huffman.adb
@@ -1,5 +1,5 @@
-with Ada.Text_IO, Ada.Unchecked_Deallocation;
-use Ada.Text_IO;
+with Ada.Text_IO, Ada.Unchecked_Deallocation; use Ada.Text_IO;
+with Ada.Streams.Stream_IO; use Ada.Streams.Stream_IO;
 
 package body Huffman is
     
@@ -11,5 +11,54 @@ package body Huffman is
     
     procedure Libere_Noeud is new Ada.Uncheck_Deallocation(Noeud,Arbre);
 
+    function Creer_Feuille(C: Character ) return Arbre is
+    begin
+        return new Noeud'(Val => C,
+                            Fg => null,
+                            Fd => null);
+    end Creer_Feuille;
+
+
+    function Creer_Arbre(Nom_Fichier : String) return Arbre is 
+       
+        type Tableau_Character is array(Character range <>) of Integer;
+        Fichier : Ada.Streams.Stream_IO.File_Type;
+        Flux : Ada.Streams.Stream_IO.Stream_Access;
+        C : Character;
+        F : File;
+        Tab : Tableau_Character(Character'First..Character'Last) := (others => 0);
+   
+    begin
+
+        F := CreeFile ;
+        -- Overture d'un fichier texte
+        begin
+            Open(Fichier, In_File, Nom_Fichier);
+        exception 
+            when others =>
+            Put("Erreur en lecture: ");  Put(Nom_Fichier); Put_Line(" n'exist pas"); 
+            return;
+        end;
+     
+        Flux := Stream(Fichier);
+        
+        -- Lecture de caractère
+        while not End_Of_File(Fichier) loop
+            C := Character'Input(Flux);
+            Tab(C) := Tab(C)+1;
+        end loop;
+
+        -- Creer File_priorite à partir de Tab
+        for I in Tab'Range loop
+            if not Tab(I)=0 
+            then
+                Entrer(F, Creer_Feuille(I), Tab(I));
+            end if;
+        end loop; 
+
+        -- Creer Arbre à partir de File_priorite F
+        Creer_Arbre(F);
+
+    end Creer_Arbre;
 
 end Huffman;
--- huffman.ads
+++ huffman.ads
@@ -1,30 +1,42 @@
-with Ada.Text_Io; use Ada.Text_Io;
-with file_priorite
+with Ada.Text_IO; use Ada.Text_IO;
+with file_priorite; use file_priorite;
+with Ada.Streams.Stream_IO; use Ada.Streams.Streams_IO;
 
+-- Ce package permets contruit une arbre Huffman à partir 
+-- d'un fichier des caractères ou d'un fichier compressé.
 generic 
     type Element is private;
     with procedure Put(e: in Element);
-    with function ">" (A, B: Element) return Boolean;
 
 package Huffman is 
 
     type Arbre is private;
+
 
     procedure Libere_Arbre(A : in out Arbre);
     
     -- Return Arbre_Vide
     function Arbre_Vide return Arbre;
 
-    
-    -- contruire une arbre avec deux fils A, B
+    -- Creer une Noeud de Feuille qui contient caractere C
+    function Creer_Feuille(C: Character) return Arbre;
+
+    -- contruire une arbre avec fils gauche A et fils droit B
     -- renvoie la racine
-    -- cette fonction est utile pour Creer_Arbre()
+    -- cette fonction est utile pour Creer_Arbre(File) à partir d'un File_priorté
     function Creer_Arbre(A, B: Arbre) return Arbre;
     
     -- contruire une arbre à partir d'une file_priorité
+    -- cette fonction est utile pour Creer_Arbre(String) à partir d'un nom d'un fichier
     function Creer_Arbre(F : in File ) return Arbre;
+    
+    -- contruire une arbre à partir de nom d'un fichier
+    -- Exception: file n'exist pas
+    function Creer_Arbre(Nom_Fichier : in String) return Arbre;
 
+    function Creer_Arbre() return Arbre;
     -- pour tester 
+    -- afficher comment ? lire en Test_Arbre
     procedure Afficher(A : in Arbre);
     
 
